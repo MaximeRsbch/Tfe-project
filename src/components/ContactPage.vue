c32e4355be57c76aa3d5c6ae1f7a11098ec71407
Maxime <maxime.rossbach@std.heh.be>
2023-12-06 12:28:36 +0100

    Possibilité de créer un formulaire de contact a l'admin

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -1,4 +1,28 @@
-<script setup></script>
+<script setup>
+import { computed, onMounted, ref } from "vue";
+import jwtDecode from "jwt-decode";
+import { useTicketsModStore } from "../stores/ticketsmod.js";
+
+const ticketsModStore = useTicketsModStore();
+
+onMounted(() => {
+    ticketsModStore.fetchTickets();
+});
+
+//Empecher l'accès à la page si l'utilisateur n'est pas connecté
+const isConnect = computed(() => localStorage.getItem("savedToken"));
+
+const tokenDecode = computed(() => jwtDecode(isConnect.value));
+
+const id = computed(() => tokenDecode.value.id_user);
+
+const title = ref("");
+const description = ref("");
+
+const createContact = () => {
+    ticketsModStore.createTickets(title.value, description.value, id.value);
+};
+</script>
 
 <template>
     <div class="flex justify-center">
@@ -21,21 +45,11 @@
                     <div class="flex-1">
                         <label
                             class="block mb-2 text-sm text-gray-600 dark:text-gray-200"
-                            >Votre pseudo</label
+                            >Titre de votre demande</label
                         >
                         <input
+                            v-model="title"
                             type="text"
-                            class="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-                        />
-                    </div>
-
-                    <div class="flex-1 mt-6">
-                        <label
-                            class="block mb-2 text-sm text-gray-600 dark:text-gray-200"
-                            >Votre email</label
-                        >
-                        <input
-                            type="email"
                             class="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                         />
                     </div>
@@ -46,6 +60,7 @@
                             >Message</label
                         >
                         <textarea
+                            v-model="description"
                             minlength="20"
                             maxlength="500"
                             class="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
@@ -54,6 +69,7 @@
                     </div>
 
                     <button
+                        @click="createContact()"
                         class="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50"
                     >
                         Envoyer votre demande
---
+++ src/stores/ticketsmod.js
@@ -0,0 +1,54 @@
+import { defineStore } from "pinia";
+import { CapacitorHttp } from "@capacitor/core";
+
+export const useTicketsModStore = defineStore("ticketsMod", {
+    state: () => ({
+        ticketsdict: {},
+    }),
+
+    getters: {
+        getTickets: (state) => Object.values(state.ticketsdict),
+        getTicketsById: (state) => (id) => state.ticketsdict[id],
+    },
+
+    actions: {
+        async fetchTickets() {
+            const response = await CapacitorHttp.request({
+                method: "GET",
+                url: "http://localhost:3000/api/ticketsmod/",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }).then((res) => {
+                this.ticketsdict = res.data;
+                console.log(this.ticketsdict);
+            });
+        },
+
+        async createTickets(
+            title,
+            description,
+            ref_user,
+            ref_commentArticles,
+            ref_commentAttr
+        ) {
+            const response = await CapacitorHttp.request({
+                method: "POST",
+                url: "http://localhost:3000/api/ticketsmod/",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                data: {
+                    title,
+                    description,
+                    ref_user,
+                    ref_commentArticles,
+                    ref_commentAttr,
+                },
+            }).then((res) => {
+                this.ticketsdict = res.data;
+                console.log(this.ticketsdict);
+            });
+        },
+    },
+});
