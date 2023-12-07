902b1b7114812c42e4d2bf45a75203053aa762ae
Maxime <maxime.rossbach@std.heh.be>
2023-12-07 15:26:06 +0100

    Page bestAttra plus belle

--- src/components/attractions/BestAttraPage.vue
+++ src/components/attractions/BestAttraPage.vue
@@ -81,21 +81,21 @@ const sortTable = (field) => {
 </script>
 <template>
     <div>
-        <div>
-            <h1 class="text-center">Best Attractions</h1>
-            <p class="text-center">
-                Here are the best attractions in the world. Click on the
-                attraction to see more details.
+        <div class="pt-10">
+            <h1 class="text-center text-4xl">Best Attractions</h1>
+            <p class="text-center text-lg pt-2">
+                Ici se trouve le classement des meilleurs attractions de chaque
+                parc
             </p>
         </div>
         <div class="pt-10">
-            <h2 class="text-center">Choisissez le parc voulu :</h2>
+            <h2 class="text-center text-lg">Choisissez le parc voulu :</h2>
             <div class="flex justify-center">
                 <select
                     v-model="parc"
                     @change="choixParc"
                     id="parc"
-                    class="block px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
+                    class="block px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-[#344d59] focus:ring-[#344d59] focus:ring-opacity-40 focus:outline-none focus:ring"
                 >
                     <option v-for="data in parcs" :id="data.id">
                         {{ data.nom }}
@@ -115,20 +115,6 @@ const sortTable = (field) => {
                             <table class="min-w-full divide-y divide-gray-300">
                                 <thead class="bg-gray-50">
                                     <tr>
-                                        <th
-                                            scope="col"
-                                            class="py-3.5 pl-2 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-                                        >
-                                            <span
-                                                class="group inline-flex text-base"
-                                            >
-                                                Id de l'attraction
-                                                <span
-                                                    class="invisible ml-2 flex-none rounded-md text-gray-400 group-hover:visible group-focus:visible"
-                                                >
-                                                </span>
-                                            </span>
-                                        </th>
                                         <th
                                             scope="col"
                                             class="px-3 py-3.5 text-left text-base font-semibold text-gray-900"
@@ -187,11 +173,6 @@ const sortTable = (field) => {
                                         :key="data.id"
                                         class="hover:bg-gray-100"
                                     >
-                                        <td
-                                            class="whitespace-nowrap py-4 px-3 text-base"
-                                        >
-                                            {{ data.id }}
-                                        </td>
                                         <div>
                                             <td
                                                 class="whitespace-nowrap px-3 py-4 text-base"
@@ -201,9 +182,16 @@ const sortTable = (field) => {
                                         </div>
 
                                         <td
+                                            v-if="data.is_open == 0"
                                             class="whitespace-nowrap px-3 py-4 text-base"
                                         >
-                                            {{ data.is_open }}
+                                            Fermée
+                                        </td>
+                                        <td
+                                            v-if="data.is_open == 1"
+                                            class="whitespace-nowrap px-3 py-4 text-base"
+                                        >
+                                            Ouverte
                                         </td>
                                         <td
                                             class="whitespace-nowrap px-3 py-4 text-base"
