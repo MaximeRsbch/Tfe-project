a16b8cb279f131fb2b1fed583d232a34030741c7
Maxime <maxime.rossbach@std.heh.be>
2023-12-07 15:28:50 +0100

    Formulaire de contact

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -28,49 +28,45 @@ const createContact = () => {
     <div class="flex justify-center">
         <div class="mt-8 lg:w-1/2 lg:mx-6">
             <div
-                class="w-full px-8 py-10 mx-auto overflow-hidden bg-white shadow-2xl rounded-xl dark:bg-gray-900 lg:max-w-xl"
+                class="w-full px-8 py-10 mx-auto overflow-hidden bg-white shadow-2xl rounded-xl lg:max-w-xl"
             >
-                <h1
-                    class="text-xl font-medium text-gray-700 dark:text-gray-200"
-                >
+                <h1 class="text-xl font-medium text-gray-700">
                     Formulaire de contact
                 </h1>
 
-                <p class="mt-1 text-gray-500 dark:text-gray-400">
+                <p class="mt-1 text-gray-500">
                     Vous pouvez nous contacter en remplissant le formulaire
                     ci-dessous.
                 </p>
 
                 <form class="mt-4">
                     <div class="flex-1">
-                        <label
-                            class="block mb-2 text-sm text-gray-600 dark:text-gray-200"
+                        <label class="block mb-2 text-sm text-gray-600"
                             >Titre de votre demande</label
                         >
                         <input
                             v-model="title"
                             type="text"
-                            class="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                            class="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
                         />
                     </div>
 
                     <div class="w-full mt-6">
-                        <label
-                            class="block mb-2 text-sm text-gray-600 dark:text-gray-200"
+                        <label class="block mb-2 text-sm text-gray-600"
                             >Message</label
                         >
                         <textarea
                             v-model="description"
                             minlength="20"
                             maxlength="500"
-                            class="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                            class="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
                             placeholder="Message"
                         ></textarea>
                     </div>
 
                     <button
                         @click="createContact()"
-                        class="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50"
+                        class="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-[#344d59] rounded-md hover:bg-[#344d59] focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50"
                     >
                         Envoyer votre demande
                     </button>
