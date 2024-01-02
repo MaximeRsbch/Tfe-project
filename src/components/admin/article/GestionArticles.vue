4933793a8b63dcaf88feb92be4fd2fc2f26d2f14
Maxime <maxime.rossbach@std.heh.be>
2024-01-02 12:56:53 +0100

    Choix d'un parc dans gestion des articles

--- src/components/admin/article/GestionArticles.vue
+++ src/components/admin/article/GestionArticles.vue
@@ -1,13 +1,19 @@
 <script setup>
 import { useArticlesStore } from "../../../stores/articles.js";
+import { useParcsStore } from "../../../stores/parcs.js";
+import { useUsersStore } from "../../../stores/users.js";
 import { computed, ref, onMounted } from "vue";
 import jwtDecode from "jwt-decode";
 import "../../../style/BulleTexte.css";
 
 const articlesStore = useArticlesStore();
+const parcsStore = useParcsStore();
+const usersStore = useUsersStore();
 
 onMounted(() => {
+    parcsStore.fetchParcs();
     articlesStore.fetchArticles();
+    usersStore.fetchModoParc();
 });
 
 const articles = computed(() => articlesStore.getArticles);
@@ -18,9 +24,42 @@ const tokenDecode = computed(() => jwtDecode(isConnect.value));
 
 const role = tokenDecode.value.role;
 
+const parc = ref("");
+const article = ref("");
+
+const parcs = computed(() => parcsStore.getParcs);
+
+const choixParc = () => {
+    //On récup l'id du parc choisit dans le select
+    const idParc =
+        document.getElementById("parc").options[
+            document.getElementById("parc").selectedIndex
+        ].id;
+
+    console.log(idParc);
+
+    //On récup les attractions du parc choisit
+    articlesStore.fetchAllArticles(idParc);
+
+    setTimeout(() => {
+        const articles = computed(() => articlesStore.getArticles);
+
+        article.value = articles.value;
+    }, 200);
+};
+
 const deleteArticle = (id) => {
     articlesStore.deleteArticle(id);
 };
+
+const usersModoParc = ref("");
+
+setTimeout(() => {
+    const users = computed(() => usersStore.getModoParc);
+    for (const user of users.value) {
+        usersModoParc.value = user.ref_parc;
+    }
+}, 300);
 </script>
 <template>
     <div class="container mx-auto">
@@ -36,13 +75,34 @@ const deleteArticle = (id) => {
                 </button>
             </div>
         </div>
+
         <div v-if="role === 'admin' || role === 'modo'">
             <div v-if="articles.length > 0">
                 <h1 class="text-center pt-10 text-4xl">Gestion des articles</h1>
                 <p class="text-center pb-10 text-2xl">
                     Ici sont affichés tous les articles pour les gérer
                 </p>
-                <div class="mt-4 flex flex-col container mx-auto">
+                <div class="pt-10">
+                    <h2 class="text-center text-lg">
+                        Choisissez le parc voulu :
+                    </h2>
+                    <div class="flex justify-center">
+                        <select
+                            v-model="parc"
+                            @change="choixParc"
+                            id="parc"
+                            class="block px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-[#344d59] focus:ring-[#344d59] focus:ring-opacity-40 focus:outline-none focus:ring"
+                        >
+                            <option v-for="data in parcs" :id="data.id">
+                                {{ data.nom }}
+                            </option>
+                        </select>
+                    </div>
+                </div>
+                <div
+                    v-if="parc !== ''"
+                    class="mt-4 flex flex-col container mx-auto"
+                >
                     <div class="overflow-x-auto">
                         <div
                             class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8"
@@ -132,23 +192,38 @@ const deleteArticle = (id) => {
                                     >
                                         <!--Affiche les info de tous les users-->
                                         <tr
-                                            v-for="article in articles"
-                                            :key="article.id"
+                                            v-for="data in article"
+                                            :key="data.id"
                                             class="hover:bg-gray-100"
                                         >
                                             <td
+                                                v-if="
+                                                    data.ref_parc ===
+                                                        usersModoParc ||
+                                                    role === 'admin'
+                                                "
                                                 class="whitespace-nowrap py-4 px-3 text-base"
                                             >
-                                                {{ article.id }}
+                                                {{ data.id }}
                                             </td>
                                             <div>
                                                 <td
+                                                    v-if="
+                                                        data.ref_parc ===
+                                                            usersModoParc ||
+                                                        role === 'admin'
+                                                    "
                                                     class="whitespace-nowrap px-3 py-4 text-base"
                                                 >
-                                                    {{ article.title }}
+                                                    {{ data.title }}
                                                 </td>
                                             </div>
                                             <td
+                                                v-if="
+                                                    data.ref_parc ===
+                                                        usersModoParc ||
+                                                    role === 'admin'
+                                                "
                                                 class="whitespace-nowrap px-3 py-4 text-base"
                                             >
                                                 <textarea
@@ -156,20 +231,23 @@ const deleteArticle = (id) => {
                                                     rows="3"
                                                     class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
                                                     >{{
-                                                        article.content
+                                                        data.content
                                                     }}</textarea
                                                 >
                                             </td>
 
                                             <td
-                                                v-if="role === 'admin'"
+                                                v-if="
+                                                    role === 'admin' ||
+                                                    role === 'modoParc' ||
+                                                    data.ref_parc ===
+                                                        usersModoParc
+                                                "
                                                 class="whitespace-nowrap px-3 py-4 text-base"
                                             >
                                                 <button
                                                     @click="
-                                                        deleteArticle(
-                                                            article.id
-                                                        )
+                                                        deleteArticle(data.id)
                                                     "
                                                     type="button"
                                                 >
@@ -197,13 +275,13 @@ const deleteArticle = (id) => {
                                                     >
                                                         <RouterLink
                                                             v-if="
-                                                                article.id !==
+                                                                data.id !==
                                                                 undefined
                                                             "
                                                             v-bind:to="{
                                                                 name: 'modifArticle',
                                                                 params: {
-                                                                    id: article.id,
+                                                                    id: data.id,
                                                                 },
                                                             }"
                                                         >
@@ -226,6 +304,9 @@ const deleteArticle = (id) => {
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="text-center pt-10 text-xl" v-if="parc == ''">
+                    Aucun parc n'a été sélectionné
                 </div>
             </div>
         </div>
